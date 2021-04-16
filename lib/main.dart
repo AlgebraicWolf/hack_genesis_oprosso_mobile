ef7a2f78543897e2f942b242cd6e1bc247cb0d92
Aleksei Volkov <volkov.aa@phystech.edu>
2021-04-16 20:37:56 +0300

    Events are now being captured

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
@@ -47,19 +48,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -68,9 +56,17 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return WebView(
-      initialUrl: 'https://youtube.com/',
-      javascriptMode: JavascriptMode.unrestricted,
+    return Listener(
+      child: WebView(
+        initialUrl: 'https://youtube.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+      onPointerDown: (event) =>
+          print("Press at (${event.position.dx}, ${event.position.dy})"),
+      onPointerUp: (event) =>
+          print("Release at (${event.position.dx}, ${event.position.dy})"),
+      onPointerMove: (event) =>
+          print("Move at  at (${event.position.dx}, ${event.position.dy})"),
     );
   }
 }
