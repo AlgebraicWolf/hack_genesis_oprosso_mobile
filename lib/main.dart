4dac5012358ae72a4149961b3945b3fad69ab8a5
Aleksei Volkov <volkov.aa@phystech.edu>
2021-04-17 01:03:03 +0300

    Basic screen recording is in place

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_screen_recording/flutter_screen_recording.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,25 +23,102 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StartRecording(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class StartRecording extends StatefulWidget {
+  StartRecording({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _StartRecordingState createState() => _StartRecordingState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _StartRecordingState extends State<StartRecording> {
+  bool isRecording = false;
+
+  Future<bool> startScreenRecord(String filename) async {
+    bool result = await FlutterScreenRecording.startRecordScreen(filename);
+    return result;
+  }
+
+  Future<String> stopScreenRecord() async {
+    String path = await FlutterScreenRecording.stopRecordScreen;
+    return path;
+  }
+
+  void printOnArrival(Future<String> str) async {
+    str.then((s) {
+      print(s);
+    });
+  }
+
+  void requestPermissions() async {
+    await PermissionHandler().requestPermissions([
+      PermissionGroup.storage,
+      PermissionGroup.photos,
+      PermissionGroup.microphone,
+    ]);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Testers gonna test"),
+      ),
+      body: Container(
+        child: ElevatedButton(
+          onPressed: () {
+            if (!isRecording) {
+              startScreenRecord("demo");
+              setState(() {
+                isRecording = true;
+              });
+            } else {
+              Future path = stopScreenRecord();
+              printOnArrival(path);
+              setState(() {
+                isRecording = false;
+              });
+            }
+          },
+          child: Text(
+            !isRecording ? "Start recording" : "Stop recording",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        alignment: Alignment.center,
+      ),
+    );
+  }
+}
+
+// First portion of settings
+class FirstSetup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Second portion of settings
+class SecondSetup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
